bc2db9ded6dbafd300c52b89423b04dfd5b75ef3
TalShinkar <tal147@gmail.com>
2019-06-15 11:43:23 +0300

    Setup Play 2 Framework

---
+++ NotebookServer/target/scala-2.12/routes/main/router/RoutesPrefix.scala
@@ -0,0 +1,15 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/Users/talshinkar/Google Drive/Technion/Semester H/AndroidNotebook/NotebookServer/conf/routes
+// @DATE:Sat Jun 15 11:40:46 IDT 2019
+
+
+package router {
+  object RoutesPrefix {
+    private var _prefix: String = "/"
+    def setPrefix(p: String): Unit = {
+      _prefix = p
+    }
+    def prefix: String = _prefix
+    val byNamePrefix: Function0[String] = { () => prefix }
+  }
+}
